e0871868e3bd743b7a3906100d99cb9e1f3c94ff
Peter André Busch <peter@pox.no>
2020-08-07 17:27:10 +0200

    Endret til Oslo

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
         <h2>Velkommen til Norge.no</h2>
         <?php
         echo "<p>Hovedstaden i Norge er: ";
-        echo "<strong>Bergen</strong>";
+        echo "<strong>Oslo</strong>";
         echo "<br />";
+        phpinfo();
         ?>
 
         I dag er det den <?php echo date("d.m.Y");?>
